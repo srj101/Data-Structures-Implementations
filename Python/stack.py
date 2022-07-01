7abedc9ddd4aded01ecae3c99d6ea42b3178fd68
srj101 <salim15-3117@diu.edu.bd>
2022-07-01 14:48:16 +0600

    dfs graph using python

---
+++ Python/stack.py
@@ -0,0 +1,20 @@
+g = {
+    "a": ["b","c"],
+    "b": ["d"],
+    "d": ["f"],
+    "c": ["e"],
+    "e": [],
+    "f":[]
+}
+
+def dfs(g,s):
+    m = []
+    stack = [s]
+    while len(stack)>0:
+        m.append(stack[0])
+        for j in g[stack[0]]:
+            stack.append(j)
+        stack.pop(0)
+    return m
+
+print(dfs(g,"a"))
